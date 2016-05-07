311467444e3560d4b6012eb7650d9699cbe27a94
virgofreedom <rattana.neak@gmail.com>
2016-05-07 04:13:52 -0700

    update js
    Update JavaScript function to make it more dynamic.

--- index.php
+++ index.php
@@ -11,12 +11,15 @@ include "includes/header.php";
 </tr>
 <?php
 for ($i=0;$i < $count_item;$i++){
+   
     echo '
     <tr>
         <td>'. $items[$i]->name .'</td>
         <td>'. $items[$i]->price .'</td>
         <td><input  type="number" min="0" id="'. $items[$i]->qty_form .'" 
-        onkeyup="get_sub('."'".$items[$i]->qty_form."',"."'".$items[$i]->price."',"."'".$items[$i]->qty_form."s'".');sumItem();" 
+        onkeyup="
+        get_sub('."'".$items[$i]->qty_form."',"."'".$items[$i]->price."',"."'".$items[$i]->qty_form."s'".');
+        total_item('."'".$count_item."',"."'sumItem','total','topping'".');" 
         name="'. $items[$i]->qty_form .'"> </td>
         <td><span id="'. $items[$i]->qty_form .'s"   align="center" style="color:red"></span></td>
     </tr>';
@@ -26,7 +29,12 @@ for ($i=0;$i < $count_item;$i++){
 <tr>
     <td>Topping :</td>
     <td>
-        <select id="topping" name="topping" onchange="sumItem();">
+        <?php
+        echo '
+        <select id="topping" name="topping" onchange="total_item('."'".$count_item."',"."'sumItem','total','topping'".');">
+        ';
+        ?>
+        
             <option value="0"></option>
             <option value="0.5">Chese</option>
             <option value="0.75">Honey Mustard</option>
@@ -38,7 +46,7 @@ for ($i=0;$i < $count_item;$i++){
 <td style="color:green">Total Selected Items</td>
 <td id="sumItem">0</td>
 <td style="color:green">Your Total Will be</td>
-<td><span name='ts' id='total' style="color:red" oninput="total()" >0</span></td>
+<td><span name='ts' id='total' style="color:red">0</span></td>
 </tr>
 <tr>
     <td></td>
--- js/GetTotal.js
+++ js/GetTotal.js
@@ -2,17 +2,38 @@ function get_sub(qty,price,res) {
     var num_qty = document.getElementById(qty).value;
     document.getElementById(res).innerHTML = Number(num_qty) * Number(price);
 }
-function  total(qty) {
+function total_item(qty,res,res_sub,top) {
     
+
+    var num_qty = document.getElementsByTagName("input");
+    var total=0;
+    var sub_total = 0;
+    for (i = 0; i < qty; i++) {
+        var sub_id = num_qty[i].id + "s";
+        total += Number(num_qty[i].value);
+        sub_total += Number(document.getElementById(sub_id).innerHTML);
+    }
+    document.getElementById(res).innerHTML = total;
+    
+    document.getElementById(res_sub).innerHTML = sub_total;
+    var toping = document.getElementById(top).value;
+    if (toping != 0){
+        sub_top(top,res_sub);
+    }
+}
+function sub_top(id,res){
+    document.getElementById(res).innerHTML = 
+    Number(document.getElementById(res).innerHTML)+
+    Number(document.getElementById(id).value)    
 }
 
-    function sumItem(){//sum for items
+function sumItem(){//sum for items
      var ft = Number(document.getElementById("fq").value);
         var pt = Number(document.getElementById("pq").value);
         var ct = Number(document.getElementById("cq").value);
         var bt = Number(document.getElementById("bq").value);
         var rt = Number(document.getElementById("rq").value);
-        var sumItem = ft + pt + ct + bt + rt;        
+        var sumItem = ft + pt + ct + bt + rt;   
         var tft = Number(document.getElementById("fqs").innerHTML);
         var tpt = Number(document.getElementById("pqs").innerHTML);
         var tct = Number(document.getElementById("cqs").innerHTML);
@@ -23,6 +44,7 @@ function  total(qty) {
         document.getElementById("sumItem").innerHTML = sumItem;
         document.getElementById("total").innerHTML = total;        
     }
+    
     /*
     function totalCharge(){//sum for charge
         
